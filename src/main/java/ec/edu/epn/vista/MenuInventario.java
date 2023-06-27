32fa0d31e3236b3209202935acb9bec3c47e3679
Jackson Ramón <116320221+jacks8n-r@users.noreply.github.com>
2023-06-27 18:28:34 -0500

    Update MenuInventario, code refactoring

--- src/main/java/ec/edu/epn/vista/MenuInventario.java
+++ src/main/java/ec/edu/epn/vista/MenuInventario.java
@@ -40,4 +40,63 @@ public class MenuInventario {
         }
         return false;
     }
+
+    public void actualizarInfoProducto(){
+        System.out.println("***************ACTUALIZAR INFORMACIÓN DE UN PRODUCTO***************");
+        opcionesMenu += "1. Actualizar nombre";
+        opcionesMenu += "\n2. Actualizar precio";
+        opcionesMenu += "\n3. Actualizar distribuidor";
+        opcionesMenu += "\n4. Actualizar unidades";
+        opcionesMenu += "\nSeleccione una opción:";
+        System.out.println(opcionesMenu);
+        int opcion = Integer.parseInt(entrada.nextLine());
+        ejecutarActualizaciones(opcion);
+    }
+
+    public boolean ejecutarActualizaciones(int opcion) {
+        String codigoA = "";
+        switch (opcion){
+            case 1:
+                System.out.println("Ingrese el código del producto:");
+                codigoA = entrada.nextLine();
+                System.out.println("Ingrese el nuevo nombre del producto:");
+                String nombreA = entrada.nextLine();
+                con.updateName(codigoA, nombreA);
+                System.out.println("La información ha sido actualizada correctamente");
+                m.inventario();
+                return true;
+            case 2:
+                System.out.println("Ingrese el código del producto:");
+                codigoA = entrada.nextLine();
+                System.out.println("Ingrese el nuevo precio del producto:");
+                double precioA = Double.parseDouble(entrada.nextLine());
+                con.updatePrecio(codigoA, precioA);
+                System.out.println("La información ha sido actualizada correctamente");
+                m.inventario();
+                return true;
+            case 3:
+                System.out.println("Ingrese el código del producto:");
+                codigoA = entrada.nextLine();
+                System.out.println("Ingrese el nuevo distribuidor del producto:");
+                String distribuidorA = entrada.nextLine();
+                con.updateDistribuidor(codigoA, distribuidorA);
+                System.out.println("La información ha sido actualizada correctamente");
+                m.inventario();
+                return true;
+            case 4:
+                System.out.println("Ingrese el código del producto:");
+                codigoA = entrada.nextLine();
+                System.out.println("Ingrese las nuevas unidades para el producto:");
+                int unidadesA = Integer.parseInt(entrada.nextLine());
+                int unidadesE = con.readUnidades(codigoA);
+                con.updateUnidades(codigoA, (unidadesE + unidadesA));
+                System.out.println("La información ha sido actualizada correctamente");
+                m.inventario();
+                return true;
+            default:
+                System.out.println("Opción no válida");
+                m.inventario();
+                return false;
+        }
+    }
 }
