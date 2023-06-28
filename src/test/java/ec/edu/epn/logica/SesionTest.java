4dec9dcff7205b402f74d4cbcf444b5b3505fb70
KevinCndor <kevin.condor@epn.edu.ec>
2023-06-27 19:00:17 -0500

    Add unit test

--- src/test/java/ec/edu/epn/logica/SesionTest.java
+++ src/test/java/ec/edu/epn/logica/SesionTest.java
@@ -1,8 +1,11 @@
 package ec.edu.epn.logica;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class SesionTest {
+    @Test
     public void given_the_credentials_when_login_then_ok(){
         Sesion s = new Sesion();
         assertEquals("admin",s.validarSesion("admin","admin123"));
